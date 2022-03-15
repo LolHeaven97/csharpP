a5b0825c69259f01befc7f062cd6a7cab9827f1d
ASUS <ASUS@LAPTOP-OEGCDEFV>
2022-03-15 20:57:10 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        Console.Write("price:");
+        double price = double.Parse(Console.ReadLine());
+        Console.Write("discount:");
+        int discount = int.Parse(Console.ReadLine());
+        price = price * (100 - discount) / 100;
+
+        Console.WriteLine("new price=" + price);
+    }
+}
---
+++ csharpP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace csharpP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // קלוט מספר המר לשלם וחשב ערך מוחלט
+            // עשה זאת עבור 4 משתנים
+            int a = Math.Abs(int.Parse(Console.ReadLine()));
+            int b = Math.Abs(int.Parse(Console.ReadLine()));
+            int c = Math.Abs(int.Parse(Console.ReadLine()));
+            int d = Math.Abs(int.Parse(Console.ReadLine()));
+            // מצא את הגדול מבין הזוג הראשון
+            int e = Math.Max(a, b);
+            // מצא את הגדול בזוג האחרון
+            int f = Math.Max(c, d);
+            // מצא את הגדול מבין שני הגדולים
+            int mx = Math.Max(e, f);
+            // חשב שורש ריבועי
+            double res = Math.Sqrt(mx);
+            // עגל את התוצאה
+            res = Math.Round(res);
+
+            Console.WriteLine("max={0} sqrt={1}", mx, res);
+            */
+            //חישוב הנחה על פי מחיר נוכחי.
+            /*
+            Console.Write("price:");
+            double price = double.Parse(Console.ReadLine());
+            Console.Write("discount:");
+            int discount = int.Parse(Console.ReadLine());
+            price = price * (100 - discount) / 100;
+
+            Console.WriteLine("new price=" + price);
+            */
+          
+        }
+    }
+}
